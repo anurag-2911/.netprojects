86d3e5e92a79204460d4e7b90502ada21406ca7a
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-05-12 00:50:00 +0530

    clean-up and tests

--- Kodelabzz.AllProjects/Kodelabzz.Library/arrays/ArraySort.cs
+++ Kodelabzz.AllProjects/Kodelabzz.Library/arrays/ArraySort.cs
@@ -1,4 +1,6 @@
-﻿namespace Kodelabzz.Library
+﻿using System.Collections.Generic;
+
+namespace Kodelabzz.Library
 {
     public class ArraySort
     {
@@ -9,47 +11,27 @@
             WorkSet granchild = new WorkSet { MasterID = "123", MasterName = "master", CurrentId = "abc" };
             WorkSet workset1 = new WorkSet { MasterID = "1234", MasterName = "schdule1", CurrentId = "ghgh" };
             WorkSet workset2 = new WorkSet { MasterID = "12345", MasterName = "schdule2", CurrentId = "gh" };
-            WorkSet[] workSets = new WorkSet[] { workset1, parent, child, granchild,workset2 };
-            int[] arr = { 12, 10, 9, 45, 2, 10, 10, 45 };
-            int n = arr.Length;
-            Dictionary<int,int> map = new Dictionary<int,int>();
-            for (int i = 0; i < n; i++)
+            WorkSet workset3 = new WorkSet { MasterID = "12345", MasterName = "schdule45", CurrentId = "ko" };
+            WorkSet workset4 = new WorkSet { MasterID = "x12345", MasterName = "schhghule45", CurrentId = "klo" };
+            WorkSet[] workSets = new WorkSet[] { workset1, workset4, workset3, workset2, granchild, child, parent };
+            //WorkSet[] workSets = new WorkSet[] { workset3, workset2, granchild, child, parent };
+
+            IGrouping<string,WorkSet>[] wortsetsWithCommonMasterID=workSets.GroupBy(item => item.MasterID).Where(g => g.Count()>1).ToArray();
+            if (wortsetsWithCommonMasterID.Length > 0)
             {
-                if (map.ContainsKey(arr[i]))
+                List<WorkSet> relatedWorksets = new List<WorkSet>();
+                foreach (var item in wortsetsWithCommonMasterID)
                 {
-                    var val = map[arr[i]];
-                    map.Remove(arr[i]);
-                    map.Add(arr[i], val + 1);
+                    relatedWorksets.AddRange(item.ToArray());
                 }
-                else
+
+                IEnumerable<WorkSet> unrelatedWorksets = workSets.Except(relatedWorksets.ToArray());
+                if (unrelatedWorksets != null && unrelatedWorksets.Any())
                 {
-                    map.Add(arr[i], 1);
+                    workSets = unrelatedWorksets.ToArray();
                 }
+               
             }
-
-            Dictionary<string,WorkSet> uniqueWorkSets = new Dictionary<string, WorkSet>();
-            foreach (var item in workSets)
-            {
-                if(!uniqueWorkSets.ContainsKey(item.MasterID))
-                {
-                    uniqueWorkSets.Add(item.MasterID, item);
-                }
-                else
-                {
-                    uniqueWorkSets.Remove(item.MasterID);
-                }
-            }
-           
-            WorkSet[] sortedList = workSets.OrderBy(si => si.MasterID).ToArray();
-            WorkSet[] distinctWorkSets = workSets.GroupBy(p => p.MasterID).Select(g => g.First()).ToArray();
-
-            WorkSet[] noParentChildWorksets= workSets.Except(distinctWorkSets).ToArray();
-           
-
-            var duplicates = arr.GroupBy(x => x)
-                  .Where(g => g.Count() > 1)
-                  .Select(y => y.Key)
-                  .ToList();
         }
     }
 
