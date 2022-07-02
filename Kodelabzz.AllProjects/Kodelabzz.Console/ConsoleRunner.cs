1dc4766dca6cd6f3b9374aeb4e9d69a0f57cce80
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-02 13:30:29 +0530

    cleanup

--- Kodelabzz.AllProjects/Kodelabzz.Console/ConsoleRunner.cs
+++ Kodelabzz.AllProjects/Kodelabzz.Console/ConsoleRunner.cs
@@ -20,8 +20,41 @@ void CallMethods()
     //BoxUnBox.Run();
 
     //StringConcat.Run();
+    int[] arr = { 1, 3, 6, 4, 1, 2 };
+    int retVal = solution(arr);
+
 }
 
+int solution(int[] A)
+{
+    int min = 1;
+
+    if (A.Length == 0)
+    {
+        return min;
+    }
+
+    Array.Sort(A);
+
+    if (A[0] > 1)
+    {
+        return min;
+    }
+    if (A[A.Length - 1] <= 0)
+    {
+        return min;
+    }
+
+    for (int i = 0; i < A.Length; i++)
+    {
+        if (A[i] == min)
+        {
+            min++;
+        }
+    }
+
+    return min;
+}
 void CallDisposeTestMethods()
 {
     Dispose_Test.Run();
